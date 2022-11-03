179e49a3a8fa15d6365c048e5acfdac909d3f93e
Vinícius Kuchnir <vina200902@gmail.com>
2022-11-02 22:02:10 -0300

    Criação das views

---
+++ src/views/Login/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Layout from '../../components/Layout';
+
+const Login = () => {
+  return (
+    <Layout>
+      <Text>Login</Text>
+    </Layout>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
---
+++ src/views/Register/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Layout from '../../components/Layout';
+
+const Register = () => {
+  return (
+    <Layout>
+      <Text>Register</Text>
+    </Layout>
+  )
+}
+
+export default Register
+
+const styles = StyleSheet.create({})
