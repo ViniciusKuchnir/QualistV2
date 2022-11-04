5352d2880a126e2df4408373bc1290dae14b3dfb
Vinícius Kuchnir <vina200902@gmail.com>
2022-11-03 21:39:00 -0300

    Criação do arquivo de rotas

--- App.js
+++ App.js
@@ -1,35 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Login, Register, Home} from './src/views';
-
-const Stack = createNativeStackNavigator();
+import Routes from './src/Routes';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register"> 
-      <Stack.Screen 
-          name='Register'
-          component={Register}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen 
-          name='Login'
-          component={Login}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Routes/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Login, Register, Home} from '../views';
+
+const Stack = createNativeStackNavigator();
+
+const Route = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Register">
+      <Stack.Screen 
+          name='Register'
+          component={Register}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name='Login'
+          component={Login}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Route;
